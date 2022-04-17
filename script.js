c064fbabff4a7911173eebe26d9c2318bc52c3ba
Kevin Wither <kvn.wither@gmail.com>
2022-04-16 20:54:33 -0600

    Header content added to html, css, and javascript

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "About";
+h2.setAttribute('id', 'about');
 
-document.querySelector("body").appendChild(h2);
+document.querySelector("div").appendChild(h2);
+
+
+const secondH2 = document.createElement("h2");
+secondH2.textContent = "Tutorial";
+secondH2.setAttribute('id', 'how-to');
+
+document.querySelector("div").appendChild(h2);
